d7376a4161fa3166945fc6c6392bcf28451085d2
elnpaath <paathr@gmail.com>
2020-12-06 20:43:59 -0800

    added submenu for portfolio in portfolio single page, but I think we need to figure out a better menu for mobile

--- src/Components/Pages/Portfolio/Portfolio.js
+++ src/Components/Pages/Portfolio/Portfolio.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import QuestionAnswer from "../../QuestionAnswer";
-import ShowcaseProject from "../../ShowcaseProject";
-import { portfolioParagraph, showcaseObjOne } from "./Data";
+import { portfolioParagraph } from "./Data";
 
 function Portfolio() {
   return (
     <>
+      <ul>
+        <li className="nav-item">
+          <Link to="/illustrations" className="nav-item">
+            Illustrations
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/branding" className="nav-item">
+            Branding
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/layout" className="nav-item">
+            Layout
+          </Link>
+        </li>
+      </ul>
+
       <QuestionAnswer {...portfolioParagraph} />
-      {/* <ShowcaseProject {...showcaseObjOne} /> */}
     </>
   );
 }

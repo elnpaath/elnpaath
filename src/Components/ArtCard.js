65e55cbe7d6049337c2fc7e8b4eedd022c2a0058
elnpaath <paathr@gmail.com>
2020-12-10 21:22:16 -0800

    added hover transform scale on >500 screen

--- src/Components/ArtCard.js
+++ src/Components/ArtCard.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./ArtCard.css";
 
 function ArtCard(dat) {
   return (
     <>
       <div className="artcard__wrapper">
-        <img
-          className="artcard-img"
-          src={process.env.PUBLIC_URL + dat.img + "cover.jpg"}
-          alt={dat.alt}
-        />
+        <Link to="/project">
+          <img
+            className="artcard-img"
+            src={process.env.PUBLIC_URL + dat.img + "cover.jpg"}
+            alt={dat.alt}
+          />
+        </Link>
         <div className="caption__wrapper">
           <h3 className="caption-title">{dat.title}</h3>
           <p className="caption">{dat.caption}</p>
